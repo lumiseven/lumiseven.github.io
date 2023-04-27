e687377d1f4c4bdfb4865eb30d3ca9adae10478f
lumiseven <lumiseven@users.noreply.github.com>
2023-04-27 11:49:00 +0000

    deploy: lumiseven/blog@898f01492aa9c22437a8bb22810894a863b3c586

---
+++ assets/doc_nginx_caddy.md.a28be017.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as s,o as n,b as l}from"./app.1dd2ad20.js";const h=JSON.parse('{"title":"Caddy","description":"","frontmatter":{},"headers":[],"relativePath":"doc/nginx/caddy.md","lastUpdated":1678419541000}'),e={name:"doc/nginx/caddy.md"},p=l("",23),c=[p];function o(t,i,d,r,C,y){return n(),s("div",null,c)}const m=a(e,[["render",o]]);export{h as __pageData,m as default};
