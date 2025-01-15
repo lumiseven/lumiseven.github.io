e9af78efa7f9e6873184c11b1dcdec0d8bc41a4b
lumiseven <lumiseven@users.noreply.github.com>
2025-01-15 03:02:48 +0000

    deploy: lumiseven/blog@a364db8b13ab5d2148d425d54ccf80cbe91e74c9

---
+++ assets/index.md.aff7eef8.lean.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,o as l,b as n}from"./app.7de298f8.js";const _=JSON.parse('{"title":"AIGC","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","lastUpdated":1733723826000}'),r={name:"index.md"},s=n("",14),i=[s];function g(p,e,a,u,A,d){return l(),t("div",null,i)}const I=o(r,[["render",g]]);export{_ as __pageData,I as default};
